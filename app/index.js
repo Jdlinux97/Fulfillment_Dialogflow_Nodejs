36a72714fcdf17953843eebe88fe7e8397ffccbb
Dimas Gonzalez <jdlinux97@gmail.com>
2019-08-04 21:24:22 -0400

    dev y pro add

--- app/index.js
+++ app/index.js
@@ -13,10 +13,16 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(routes)
 
-app.listen(process.env.PORT, process.env.IP);
+if (process.env.NODE_ENV == 'production') {
+    app.listen(process.env.PORT, () => {
+        console.log(`Service up in the port ${process.env.PORT}`)
+    })
+}
 
 // Desarrollo
 if (process.env.NODE_ENV == 'develop') {
+    app.listen(process.env.PORT, process.env.IP);
+
     const ngrok = require('ngrok');
     (async function () {
         const url = await ngrok.connect(process.env.PORT);
